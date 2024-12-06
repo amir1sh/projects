3eef41f91812c3e1af31de75d66b5dedb4d91db5
amir1sh <amir.shaiakhmedov@gmail.com>
2024-12-06 20:41:02 +0300

    Add files via upload

--- ghost game.py
+++ ghost game.py
@@ -5,17 +5,21 @@ score=0
 while feeling_brave:
     ghost_door=randint(1,3)
     print('Перед вами 3 двери')
-    print('Привидение за какой то из них')
+    print('Привидение за какой-то из них')
     print('Какую дверь вы откроете?')
     door=input('1,2 или 3: ')
     door_num=int(door)
     if door_num==ghost_door:
         print('ПРИВИДЕНИЕ!!!')
+        print('Беги от сюда!')
+        print('ИГРА ОКОНЧНА!') 
         feeling_brave=False
     else:
         print('Нет привидения, вы переходите в следующую комнату ')
         print('')
         score=score+1
-print('Беги от сюда!')
-print('ИГРА ОКОНЧНА! Ваш счет',score)
+    if score==5:
+        print('ПОЗДРАВЛЯЮ!!! ВЫ ВЫШЛИ ИЗ ЗАМКА')
+        feeling_brave=False
+print('Ваш счет',score)
 
